29323b3bf2e22a71480e03c1166e130efb2a9be8
tcweidao <2209697714@qq.com>
2020-03-12 14:13:40 +0800

    新增StorageUtils 的Cookie 操作

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -4,11 +4,10 @@
  * @author TianChao (2209697714@qq.com)
  * @time 2019/12/18
  */
-import {IStorageUtils} from "../types/IStorageUtils";
 
-class StorageUtilsImpl implements IStorageUtils {
+export class StorageUtils  {
 
-    deleteStorage(key: string): boolean {
+    public  static  deleteStorage(key: string): boolean {
         if(window.sessionStorage) {
             window.sessionStorage.removeItem(key);
             return true;
@@ -16,7 +15,7 @@ class StorageUtilsImpl implements IStorageUtils {
         return false;
     }
 
-    getStorage<T = any>(key: string, isJson?: boolean): T {
+    public  static getStorage<T = any>(key: string, isJson?: boolean): T {
         if(window.sessionStorage) {
             let result: any = window.sessionStorage.getItem(key);
             if(!!isJson === true) {
@@ -28,8 +27,36 @@ class StorageUtilsImpl implements IStorageUtils {
         }
     }
 
-    setStorage(key: string, value: any, json?: boolean): void {
+    public  static  setStorage(key: string, value: any, json?: boolean): void {
         window.sessionStorage.setItem(key, json ? JSON.stringify(value) : value);
     }
+
+    /**
+     * @param key
+     * @param value
+     * @param expires 过期时间戳 默认值 2 * 60 * 60 * 1000
+     */
+    public static setCookie(key: string, value: string, expires?): void {
+        if (!expires) {
+            expires = 2 * 60 * 60 * 1000;
+        }
+        const date = new Date(Date.now() + expires);
+        // @ts-ignore
+        document.cookie = key + "=" + value + ";expires=" + date.toGMTString();
+    }
+
+    public static removeCookie(key: string): void {
+        StorageUtils.setCookie(key, "null", -1);
+    }
+
+    public static getCookie(key: string): string {
+        const cookies = document.cookie.split(";");
+        for (const item of cookies) {
+            const cookieKeyValue = item.split("=");
+            if (cookieKeyValue[0].replace(" ", "") === key) {
+                return cookieKeyValue[1];
+            }
+        }
+        return null;
+    }
 }
-export const StorageUtils: IStorageUtils = new StorageUtilsImpl();
