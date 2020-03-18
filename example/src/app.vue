fbb2cf1728391fdd90db8447e8b9c10ce40ce2a8
tcweidao <2209697714@qq.com>
2020-03-18 16:07:57 +0800

    添加用于测试的包

---
+++ example/src/app.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="main">
+        {{ message }}
+    </div>
+</template>
+
+<script>
+/**
+ * 用于测试 project-utils 包
+ */
+import { request } from "../../dist";
+
+export default {
+    data() {
+        return {
+            message: 'test main app',
+        }
+    },
+    mounted() {
+        console.log(request);
+    }
+}
+</script>
